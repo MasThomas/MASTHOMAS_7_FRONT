4ca305dc0f5f73c94df66df37224eb8ab76ad012
Thomas <toma.mas@gmail.com>
2021-11-23 15:10:18 +0100

    adding signout handler

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,8 @@
 import "./topbar.css"
 import logo from "../../assets/icon.png"
 import defaultPicture from "../../assets/default.png"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons"
 import {Link} from "react-router-dom"
 import { useContext } from "react"
 import { AuthContext} from "../../context/AuthContext"
@@ -9,6 +11,11 @@ export default function Topbar() {
     
     const {user} = useContext(AuthContext);
 
+    function signoutHandler(e) {
+        e.preventDefault();
+        localStorage.clear();
+        window.location.reload();
+    }
 
     return (
         <div className="topbarContainer">
@@ -24,11 +31,15 @@ export default function Topbar() {
             
             <div className="topbarRight">
                 <span className="topbarProfile">
-                    <div className="profileImgContainer">
+                    <div className="profileImgContainer" title="Aller sur votre page de profl">
                         <Link to={`profile/${user.username}`}>
                             <img className="profileImg" src={user.imageUrl ? user.imageUrl : defaultPicture} alt="Accéder à votre profil"/>
+                            <span className="usernameName">{user.username}</span>
                         </Link>
                     </div>
+                <div className="signoutContainer"  title="Se déconnecter">
+                    <FontAwesomeIcon className="signoutIcon fa-lg" icon={faSignOutAlt} onClick={signoutHandler}/> 
+                </div>
                 </span>
             </div>
         </div>
