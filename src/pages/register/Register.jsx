0205bf4bf75d9ea7e7f43a4784d0f9687306aa4e
Thomas <toma.mas@gmail.com>
2021-12-19 15:14:16 +0100

    Ending register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,10 @@
 import { useRef } from "react";
-import {useNavigate } from "react-router-dom"
+import {useNavigate} from "react-router-dom"
 import axios from "axios";
 import "./register.css"
 import { Link } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 export default function Register() {
@@ -10,6 +12,7 @@ export default function Register() {
     const username = useRef();
     const password = useRef();
     const passwordAgain = useRef();
+    const companyRole = useRef();
     const navigate = useNavigate ();
 
     const handleClick = async (e) => {
@@ -19,14 +22,22 @@ export default function Register() {
         } else {
             const user = {
                 username: username.current.value,
+                companyRole: companyRole.current.value,
                 email: email.current.value,
-                password: password.current.value
+                password: password.current.value,
+                avatar: "imagesdefault/defaultuseravatar.png"
             }
             try {
-                await axios.post("/users/register", user);
-                navigate("/login")
+                await axios.post("/users/signup", user)
+                .then(function (res) {
+                const validate = (res.data.message)
+                toast(validate)
+                setTimeout(() => {navigate("/login")}, 3000);
+                })
             } catch (err) {
                 console.log(err)
+                const err2 = (err.response.data.error)
+                toast(err2)
             };
         }
     };
@@ -41,12 +52,24 @@ export default function Register() {
                 </div>
                 <div className="loginRight">
                     <form className="loginBox" onSubmit={handleClick}>
-                        <input placeholder="Email" required ref={email} type='email' className="loginInput" />
-                        <input placeholder="Nom d'utilisateur" required ref={username} className="loginInput" />
-                        <input placeholder="Mot de passe" required ref={password} type='password' minLength="8" className="loginInput" />
-                        <input placeholder="Saisissez à nouveau votre mot de passe" type='password' minLength="8" required ref={passwordAgain} className="loginInput" />
+                        <input placeholder="Email" required ref={email} type='email' className="registerInput" />
+                        <input placeholder="Nom d'utilisateur" required ref={username} className="registerInput" />
+                        <input placeholder="Votre poste dans la société" required ref={companyRole} type="text" className="registerInput" />
+                        <input placeholder="Mot de passe" required ref={password} type='password' minLength="8" className="registerInput" />
+                        <input placeholder="Saisissez à nouveau votre mot de passe" type='password' minLength="8" required ref={passwordAgain} className="registerInput" />
                         <button className="loginButton" type="submit">S'inscrire</button>
                         <Link to ="/login" className="loginRegisterButton">Se connecter</Link>
+                        <ToastContainer
+                            position="top-right"
+                            autoClose={3000}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover={false}
+                        />
                     </form>
                 </div>   
             </div>            
