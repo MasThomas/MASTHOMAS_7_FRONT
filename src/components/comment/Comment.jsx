e23a61325572657cc3bb8b4425ad11615a435e86
Thomas <toma.mas@gmail.com>
2021-12-23 14:35:58 +0100

    new comment component

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import "./comment.css"
 
-export default function Comment({comment, username}) {
+export default function Comment({comment, username, profileImg}) {
     return (
     <div className="commentWrapper">
-        {username}
-        {comment}
+        <span className='userInfos'>
+            <img src={profileImg} alt='profileImg' className="commentProfileImg"></img>
+            <NavLink to={`/profile/${username}`} className="commentUserUsername">{username}</NavLink>
+        </span>
+            {comment}
     </div>
     )
 }
