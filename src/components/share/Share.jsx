d0d1f4374e6744ff812376737df86b5133f2f708
Thomas <toma.mas@gmail.com>
2021-12-23 14:39:00 +0100

    share modifications for multipart/form-data

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,81 +1,87 @@
 import "./share.css"
-import defaultPicture from "../../assets/default.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCamera, faWindowClose } from "@fortawesome/free-solid-svg-icons"
-import { AuthContext } from "../../context/AuthContext"
 import { useContext, useRef, useState } from "react"
+import { AuthContext } from "../../context/AuthContext"
 import axios from "axios"
+import authHeader from "../../authHeader"
+import { toast, ToastContainer } from "react-toastify";
+import FormData from "form-data"
 
 export default function Share() {
 
-    const {user} = useContext(AuthContext)
-    const description = useRef();
-    let userInformations = JSON.parse(localStorage.getItem('user'));
-    const token = userInformations.token;
+    const { user } = useContext(AuthContext)
+    const content = useRef();
 
     const [file, setFile] = useState(null)
 
     const submitHandler = async (e) => {
         e.preventDefault()
 
-        const newPost = {
-            userId : user.userId,
-            content : description.current.value,
-            imageUrl : ""
-        }
+        const form = new FormData();
+        form.append("UserId", user.userId)
 
-        if(file) {
-            const data = new FormData();
-            const fileName = Date.now() + file.name;
-            data.append("file", file);
-            data.append("name", fileName);
-            newPost.imageUrl = fileName;
-            console.log(newPost)
-            try {
-                await axios.post('/images', data)
-            } catch(err) {
-                console.log(err)
+        if(!file && !((content.current.value).length > 2)) {
+            toast("Vous ne pouvez pas poster un message vide ou trop court !")
+        } else {
+            if(file) {
+                const fileName = Date.now() + file.name
+                form.append("name", fileName)
+                form.append("file", file)
             }
-        };
-        console.log(newPost)
-        try {
-            await axios.post('/posts/', newPost, { headers: {"Authorization" : `Bearer ${token}`} });
-            window.location.reload();
-        } catch (err) {
-            console.log(err)
+            if((content.current.value).length > 2) {
+                form.append("content", content.current.value)
+            }
+            try {
+                await axios.post("/posts/", form, {headers: authHeader(), "Content-type" : "multipart/form-data"});
+                toast("Post créé avec succès !" )
+                setTimeout(() => {
+                    window.location.reload()
+                }, 2000); ;
+            } catch(error) {
+                toast(error.response.data.message)
+            
+            }
         }
     }
+
 
     return (
         <div className="share">
             <div className="shareWrapper">
-                <div className="shareTop">
-                    <img src={user.imageUrl ? user.imageUrl : defaultPicture} alt="" className="shareProfileImg" />
-                            <input 
-                            type="text"
-                            placeholder={"Que souhaitez-vous partager " + user.username + "?" }
-                            className="shareInput" 
-                            ref={description}
-                            />
-                </div>
-                <hr className="shareHr" />
-                {file && (
-                    <div className="shareImgContainer">
-                        <img className="shareImg" src={URL.createObjectURL(file)} alt=""/>
-                        <FontAwesomeIcon className="shareCancelImg" icon={faWindowClose} onClick={() => setFile(null)} /> 
+                <form className="shareBottom" onSubmit={submitHandler} encType="multipart/form-data">
+                    <div className="shareTop">
+                        <img src={user.imageUrl} alt="" className="shareProfileImg" />
+                        <input type="text" placeholder={"Que souhaitez-vous partager " + user.username + "?" } className="shareInput" ref={content}/>
                     </div>
-                )}
-                <form className="shareBottom" onSubmit={submitHandler}>
+                    <hr className="shareHr" />
+                    {file && (
+                        <div className="shareImgContainer">
+                            <img className="shareImg" src={URL.createObjectURL(file)} alt=""/>
+                            <FontAwesomeIcon className="shareCancelImg" icon={faWindowClose} onClick={() => setFile(null)} /> 
+                        </div>
+                    )}
                     <label htmlFor="file" className="shareOptions">
                         <div className="shareOption">
-                        <FontAwesomeIcon className="shareIcon" icon={faCamera} /> 
-                        <span className="shareOptionText">Ajouter une Photo</span>
-                        <input style={{display:"none"}} type="file" id="file" accept=".png,.jpg,.jpeg,.gif" onChange={(e) =>setFile(e.target.files[0])} />
+                            <FontAwesomeIcon className="shareIcon" icon={faCamera} /> 
+                            <span className="shareOptionText">Ajouter une Image</span>
+                            <input style={{display:"none"}} type="file" id="file" accept=".png,.jpg,.jpeg,.gif" onChange={(e) =>setFile(e.target.files[0])} />
                         </div>
                     </label>
                     <button className="shareButton" type="submit">Partager</button>
                 </form>
             </div>
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover={false}
+            />
         </div>
     )
 }
