b13adff0ca1d422fb6f14fa1d70228bf8f70dff5
Thomas <toma.mas@gmail.com>
2021-12-23 14:39:32 +0100

    route modification

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -3,16 +3,17 @@ import "./feed.css"
 import Share from "../share/Share"
 import Post from "../post/Post"
 import axios from "axios"
+import authHeader from "../../authHeader";
 
 export default function Feed({username}) {
+    
     const [posts, setPosts] = useState([]);
-
 
     useEffect(() => {
         const fetchPosts = async () => {
             const res = username 
-            ? await axios.get("/posts/users/"+ username)
-            : await axios.get("/posts/")
+            ? await axios.get("/posts/wall/"+ username, {headers : authHeader()})
+            : await axios.get("/posts/", {headers: authHeader()})
             setPosts(res.data.sort((p1,p2) => {
                 return new Date(p2.createdAt) - new Date(p1.createdAt); 
             }));
@@ -25,7 +26,7 @@ export default function Feed({username}) {
             <div className="feedWrapper">
                 <Share/>
                 {posts.map((p) => (
-                    <Post key={p._id} post={p}/>
+                    <Post key={p.id} post={p}/>
                 ))}
             </div>
         </div>
