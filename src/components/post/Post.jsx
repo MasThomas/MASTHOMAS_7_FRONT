66ee862f95b06fc16664c5eaf36304bc8aff9f0c
Thomas <toma.mas@gmail.com>
2021-12-23 14:37:55 +0100

    comments functionnality, toastify, finishing posts

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,36 +1,77 @@
 import "./post.css"
-import defaultPicture from "../../assets/default.png"
+import Comment from "../comment/Comment";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import TimeAgo from 'timeago-react';
+import * as timeago from 'timeago.js'
+import fr from 'timeago.js/lib/lang/fr';
 import {Link} from "react-router-dom"
-
+import authHeader from "../../authHeader";
+import { toast, ToastContainer } from "react-toastify";
+timeago.register('fr', fr);
 
 
 export default function Post({post}) {
     const [user, setUser] = useState({});
-
+    const comment = useRef()
+    const [toggleComments, setToggleComments] = useState(false)
+    
     useEffect(() => {
         const fetchUser = async () => {
-            const res = await axios.get(`/users?=${post.userId}`);
-            console.log(res)
+            const res = await axios.get(`/users/profils/id/${post.User.id}`, { headers: authHeader()});
             setUser(res.data)
         };
+
         fetchUser();
-    },[post.userId])
+    },[post])
+
+    const comments = post.Comments.map(comment => <Comment key={comment.id} comment={comment.comment} username={comment.User.username} profileImg={comment.User.imageUrl}/>)
+
+    const showCommentsHandler = async (e) => {
+        setToggleComments(previousState => !previousState)
+    }
+
+
+
+    const commentSubmitHandler = async (e) => {
+        e.preventDefault()
+        if(comment === "") {
+            toast('Veuillez indiquer un commentaire valide')
+        } else {
+            try {
+                const token = authHeader();
+                const newComment = {
+                    comment : comment.current.value,
+                    UserId: user.id, 
+                    PostId: post.id
+                }
+                await axios.post('/posts/comment/'+ post.id, newComment, {headers: token})
+                .then(function(res) {
+                    const validateComment = (res.data.message)
+                    toast(validateComment)
+                    setTimeout(() => {window.location.reload()}, 2000)
+                })
+            } catch(error){
+                toast(error)
+            }
+        }
+    }
 
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <Link className="postTopLeft" to={`profile/${user.username}`}>
-                            <img className="postProfileImg" src={user.imageUrl || defaultPicture} alt="" />
+                        <Link className="postTopLeft" to={`/profile/${user.username}`}>
+                            <img className="postProfileImg" src={user.imageUrl} alt="" />
                         </Link>
-                        <span className="postUsername">{user.username}</span>
-                        <span className="postDate"><TimeAgo date={post.createdAt}/></span>
+                        <Link className="postTopLeft" to={`/profile/${user.username}`}>
+                            <span className="postUsername">{user.username}</span>
+                            <span className="postUserRole">{user.companyRole}</span>
+                        </Link>
+                        <span className="postDate"><TimeAgo datetime={new Date(post.createdAt)} locale='fr'/></span>
                     </div>
                     <div className="postTopRight">
                     <FontAwesomeIcon className="moreIcon" icon={faEllipsisV} /> 
@@ -44,10 +85,31 @@ export default function Post({post}) {
 
                 <div className="postBottom">
                     <div className="postBottomRight">
-                        <div className="postCommentText">{post.comment} Commentaires</div>
+                        <div className="postCommentText" onClick={showCommentsHandler}>{post.Comments.length === 0 ? 'Aucun commentaire à afficher' : `Afficher ${post.Comments.length} commentaire(s)`}</div>
                     </div>
                 </div>
             </div>
+            <div className="commentContainer" style={{display: toggleComments? 'block' : 'none'}}>
+                {comments}
+            </div>
+            <div className="addComment">
+                <form className="commentInput" onSubmit={commentSubmitHandler} >
+                    <input type="text" className="shareInput" placeholder="Ajouter un commentaire" ref={comment}/>
+                    <button className="shareButton" type="submit">Envoyer le commentaire</button>
+                </form>
+            </div>
+            
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover={false}
+            />
         </div>
     )
 };
